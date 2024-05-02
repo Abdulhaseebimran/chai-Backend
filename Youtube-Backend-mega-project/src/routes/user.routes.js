8f1db77f146556d6975001ab02f9dd948ff97116
Abdulhaseebimran <haseebimran0001@gmail.com>
2024-05-02 22:27:15 +0500

    Add new routes for user controllers

--- Youtube-Backend-mega-project/src/routes/user.routes.js
+++ Youtube-Backend-mega-project/src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controllers.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetail,
+  updateAvatar,
+  updateCoverImage,
+  getUserChannelProfile,
+  getUserHistory,
+} from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -24,5 +36,14 @@ router.route("/login").post(loginUser);
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetail);
+router.route("/avater").patch(verifyJWT, upload.single("avatar"), updateAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getUserHistory);
 
 export default router;
