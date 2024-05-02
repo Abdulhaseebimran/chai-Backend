beb1ff6a9b4a53bf079ca10cb3a3528c78f09c99
Abdulhaseebimran <haseebimran0001@gmail.com>
2024-05-02 16:39:00 +0500

    channel aggregation pipeline

--- Youtube-Backend-mega-project/src/controllers/user.controllers.js
+++ Youtube-Backend-mega-project/src/controllers/user.controllers.js
@@ -341,11 +341,11 @@ const updateAvatar = asyncHandler(async (req, res) => {
     "User avatar updated successfully"
   ).select("-password");
 
+  // TODO: remove the avatar from cloudinary
+
   return res
     .status(200)
-    .json(
-      new ApiResponse(200, user, "User avatar updated successfully")
-    );
+    .json(new ApiResponse(200, user, "User avatar updated successfully"));
 });
 
 // update the user cover image
@@ -375,10 +375,84 @@ const updateCoverImage = asyncHandler(async (req, res) => {
 
   return res
     .status(200)
+    .json(new ApiResponse(200, user, "User cover image updated successfully"));
+});
+
+// create user channel profile
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params; // get the username from the params
+  if (!username?.trim()) {
+    throw new ApiError(400, "username is missing");
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscirbers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers",
+        },
+        channelSubscribedCountTo: {
+          $size: "$subscribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullname: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelSubscribedCountTo: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+      },
+    },
+  ]);
+  // console.log("channel", channel);
+  if (!channel?.length) {
+    throw new ApiError(404, "Channel not found");
+  }
+
+  return res
+    .status(200)
     .json(
-      new ApiResponse(200, user, "User cover image updated successfully")
+      new ApiResponse(
+        200,
+        channel[0],
+        "User Channel profile fetched successfully"
+      )
     );
 });
+
 export {
   registerUser,
   loginUser,
@@ -389,4 +463,5 @@ export {
   updateAccountDetail,
   updateAvatar,
   updateCoverImage,
+  generateAccessAndRefreshToken,
 };
