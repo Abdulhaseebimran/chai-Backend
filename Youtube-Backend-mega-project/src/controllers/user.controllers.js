8f090dc5a28a7aa0eff73f194c41603984e675ef
Abdulhaseebimran <haseebimran0001@gmail.com>
2024-05-02 19:36:11 +0500

    Add getwatchHistory endpoint

--- Youtube-Backend-mega-project/src/controllers/user.controllers.js
+++ Youtube-Backend-mega-project/src/controllers/user.controllers.js
@@ -453,6 +453,60 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     );
 });
 
+// get user History of watched videos
+const getUserHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        $pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              $pipeline: [
+                {
+                  $lookup: {
+                    fullname: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "watch History fetched successfully"
+      )
+    );
+});
+
 export {
   registerUser,
   loginUser,
@@ -464,4 +518,6 @@ export {
   updateAvatar,
   updateCoverImage,
   generateAccessAndRefreshToken,
+  getUserChannelProfile,
+  getUserHistory,
 };
