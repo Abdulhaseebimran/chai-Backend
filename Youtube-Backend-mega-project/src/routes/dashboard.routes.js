e8a68520ee5236e7bbc0608485921640f6a07e70
Abdulhaseebimran <haseebimran0001@gmail.com>
2024-05-03 16:34:40 +0500

    Added: All the routes for assignment

---
+++ Youtube-Backend-mega-project/src/routes/dashboard.routes.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import {
+    getChannelStats,
+    getChannelVideos,
+} from "../controllers/dashboard.controllers.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
+
+const router = Router();
+
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+router.route("/stats").get(getChannelStats);
+router.route("/videos").get(getChannelVideos);
+
+export default router
